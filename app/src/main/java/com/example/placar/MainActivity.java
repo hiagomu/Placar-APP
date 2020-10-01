4e006debb421d132aa959b7e1d3f87979fedd93b
hiago <hiagomuuu@gmail.com>
2020-10-01 17:31:33 -0300

    Third Commit

--- app/src/main/java/com/example/placar/MainActivity.java
+++ app/src/main/java/com/example/placar/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.placar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,29 +16,37 @@ public class MainActivity extends AppCompatActivity {
     private EditText time2;
     private EditText ngols;
     private Button iniciar;
-
+    private Switch onOffSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        carregaCampos();
+        switchOn();
+        cliqueBotao();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+            tempo.setText("");
+            time1.setText("");
+            time2.setText("");
+            ngols.setText("");
+            onOffSwitch.setChecked(false);
+            tempo.requestFocus();
+    }
+
+    private void carregaCampos() {
         tempo = findViewById(R.id.tempoEditText);
         time1 = findViewById(R.id.time1EditText);
         time2 = findViewById(R.id.time2EditText);
-        Switch onOffSwitch = (Switch)  findViewById(R.id.golsSwitch);
         ngols = findViewById(R.id.ngolsEditText);
         iniciar = findViewById(R.id.iniciarButton);
+        onOffSwitch  = (Switch)  findViewById(R.id.golsSwitch);
+    }
 
-        if(tempo.getText().toString().isEmpty() || Integer.parseInt(tempo.getText().toString()) ==  0){
-            tempo.setError("Insira a duração");
-        }
-        if(time1.getText().toString().isEmpty()) {
-            time1.setError("Insira o nome do time");
-        }
-        if(time2.getText().toString().isEmpty()) {
-            time2.setError("Insira o nome do time");
-        }
-
+    private void switchOn() {
         onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -52,10 +60,51 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-
-
-
     }
+
+    private boolean validaCampos() {
+        if (tempo.getText().toString().isEmpty() || Integer.parseInt(tempo.getText().toString()) == 0) {
+            tempo.setError("Insira a duração");
+            return false;
+        }
+        if (time1.getText().toString().isEmpty()) {
+            time1.setError("Insira o nome do time");
+            return false;
+        }
+        if (time2.getText().toString().isEmpty()) {
+            time2.setError("Insira o nome do time");
+            return false;
+        }
+        return true;
+    }
+
+    private void cliqueBotao() {
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!validaCampos()) return;
+
+                int nTempo = Integer.parseInt(tempo.getText().toString());
+                String nomeTime1 = time1.getText().toString();
+                String nomeTime2 = time2.getText().toString();
+                // Se o switch estiver acionado enviar numGols
+                int numGols = Integer.parseInt(ngols.getText().toString());
+
+                enviarValores(nTempo, nomeTime1, nomeTime2, numGols);
+            }
+        });
+    }
+
+    private void enviarValores(int nTempo, String nomeTime1, String nomeTime2, int numGols) {
+        Intent intent = new Intent(MainActivity.this, PartidaActivity.class);
+        intent.putExtra("Tempo", nTempo);
+        intent.putExtra("Time1", nomeTime1);
+        intent.putExtra("Time2", nomeTime2);
+        intent.putExtra("Gols", numGols);
+        startActivity(intent);
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/placar/PartidaActivity.java
+++ app/src/main/java/com/example/placar/PartidaActivity.java
@@ -2,13 +2,60 @@ package com.example.placar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PartidaActivity extends AppCompatActivity {
+
+    private TextView crono;
+    private TextView time1;
+    private TextView time2;
+
+    private TextView gol1;
+    private TextView gol2;
+    private Button bGol1;
+    private Button bGol2;
+
+    private TextView falta1;
+    private TextView falta2;
+    private Button bfalta1;
+    private Button bfalta2;
+
+    private TextView passe1;
+    private TextView passe2;
+    private Button bPasse1;
+    private Button bPasse2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partida);
+
+        carregaCampos();
+        setPreContent();
+    }
+
+    private void carregaCampos() {
+        crono = findViewById(R.id.cronoTextView);
+        time1 = findViewById(R.id.time1TextView);
+        time2 = findViewById(R.id.time2TextView);
+
+        gol1 = findViewById(R.id.gol1TextView);
+        gol2 = findViewById(R.id.gol2TextView);
+        bGol1 = findViewById(R.id.gol1Button);
+        bGol2 = findViewById(R.id.gol2Button);
+
+        passe1 = findViewById(R.id.passe1TextView);
+        passe2 = findViewById(R.id.passe2TextView);
+        bPasse1 = findViewById(R.id.passe1Button);
+        bPasse2 = findViewById(R.id.passe2Button);
+    }
+    private void setPreContent() {
+        Intent intent = getIntent();
+        time1.setText(intent.getStringExtra("Time1"));
+        time2.setText((intent.getStringExtra("Time2")));
     }
 }
